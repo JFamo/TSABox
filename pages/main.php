9bec9362941a923980269b692c3bb2ccbf3455d4
Asher Hamrick <ashhamrick@comcast.net>
2019-06-24 14:44:55 -0400

    fonts

--- pages/main.php
+++ pages/main.php
@@ -39,15 +39,12 @@ session_start();
             <div class="col nopadding">
                 <img src="../images/logo.png" class="d-inline-block verticalCenter" alt="" style="height:2.5rem;">
             </div>
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">TSABox</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown link
