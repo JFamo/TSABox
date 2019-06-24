01c3f3414dcb984889a744f6e0ef8722e25c8245
T Rohrbach <31772006+pepper866@users.noreply.github.com>
2019-06-24 10:33:58 -0400

    footer thing

--- index.php
+++ index.php
@@ -55,4 +55,13 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="js/scripts.js"></script>
   </body>
+
+  
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted"><center>Team 1285, 2019, All Rights Reserved</center>
+        </span>
+      </div>
+    </footer>
+
 </html>
