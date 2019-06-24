bedcdd764472cf6623aedefec0bd40f3c7d5dcd1
T Rohrbach <31772006+pepper866@users.noreply.github.com>
2019-06-24 10:56:22 -0400

    FONTS

--- index.php
+++ index.php
@@ -166,7 +166,7 @@ if(isset($_SESSION['username'])){
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
-
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i|Ubuntu:400,400i,700,700i&display=swap" rel="stylesheet">
     <title>TSABox</title>
   </head>
   <body>
