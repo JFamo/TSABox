dbd6450935c5a0d9adf7bdcb21d921b25d3974e4
Asher Hamrick <ashhamrick@comcast.net>
2019-06-26 17:41:11 -0400

    final touches

--- pages/admin.php
+++ pages/admin.php
@@ -17,6 +17,11 @@ session_start();
 
 $username = $_SESSION['username'];
 $rank = $_SESSION['rank'];
+
+if(!isset($passmatch)){
+  $passmatch=true;
+}
+
 require('../php/connect.php');
 $query = "SELECT chapter FROM user_chapter_mapping WHERE username='$username'";
 $resultChapter = mysqli_query($link, $query);
@@ -63,6 +68,7 @@ if(isset($_POST['newPassword']) && isset($_POST['confirmPassword'])){
     if (!$result){
       die('Error: ' . mysqli_error($link));
     }
+
     $count = mysqli_num_rows($result);
     if($count == 1){
 
@@ -75,6 +81,11 @@ if(isset($_POST['newPassword']) && isset($_POST['confirmPassword'])){
         die('Error: ' . mysqli_error($link));
       }
     }
+  }
+
+  else{
+    $passmatch=false;
+
   }
 }
 
@@ -140,6 +151,8 @@ if(isset($_POST['del'])){
 }
 
 
+if($rank == "adviser" || $rank == "admin") {
+
 ?>
 
 <!doctype html>
@@ -204,7 +217,7 @@ if(isset($_POST['del'])){
                 </li>
                 <?php if($rank == "adviser" || $rank == "admin") { ?>
                   <li class="nav-item">
-                    <a class="nav-link" href="../php/admin.php">
+                    <a class="nav-link active" href="../pages/admin.php">
                       Admin
                     </a>
                   <?php } ?>
@@ -220,7 +233,13 @@ if(isset($_POST['del'])){
             <!-- Title -->
             <div class="container" id="content">
               <h1>Admin</h1>
-              <small>Manage your chapter</small>
+              <small>Edit important chapter data</small><br>
+              <small class="text-danger">Warning! Functions of this page should be used carefully and intentionally!</small>
+              <?php if(!$passmatch){ ?>
+              <div class="alert alert-danger">
+                 Passwords do not match. Failed to reset.
+              </div>
+            <?php }?>
 
               <!-- Change User Rank -->
               <center>
@@ -493,3 +512,107 @@ if(isset($_POST['del'])){
                     </footer>
 
                     </html>
+
+                  <?php }
+
+                  else{ ?>
+                    <!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="../bootstrap-4.1.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/styles.css">
+
+  <title>TSABox</title>
+</head>
+
+<body>    
+  <!-- Nav Bar -->
+  <nav class="header bg-blue navbar navbar-expand-sm navbar-dark" style="min-height:95px; z-index: 1000;">
+    <a class="navbar-brand" href="main.php">
+      <div class="row">
+        <div class="col nopadding">
+          <img src="../images/logo.png" class="d-inline-block verticalCenter" alt="" style="height:2.5rem;">
+        </div>
+        <a class="navbar-brand" href="main.php">TSABox</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                OfficerBox
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="president.php">President</a>
+                <a class="dropdown-item" href="vice.php">Vice President</a>
+                <a class="dropdown-item" href="secretary.php">Secretary</a>
+                <a class="dropdown-item" href="treasurer.php">Treasurer</a>
+                <a class="dropdown-item" href="reporter.php">Reporter</a>
+                <a class="dropdown-item" href="parliamentarian.php">Parliamentarian</a>
+              </div>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  EventBox
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="myevents.php">My Events</a>
+                  <a class="dropdown-item" href="rules.php">Rules</a>
+                  <a class="dropdown-item" href="selection.php">Event Selection</a>
+                  <a class="dropdown-item" href="quiz.php">Interest Quiz</a>
+                </div>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    SocialBox
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="profile.php">My Profile</a>
+                    <a class="dropdown-item" href="chapter.php">My Chapter</a>
+                    <a class="dropdown-item" href="social.php">Find Friends</a>
+                  </div>
+                </li>
+                <?php if($rank == "adviser" || $rank == "admin") { ?>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../php/admin.php">
+                      Admin
+                    </a>
+                  <?php } ?>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../php/logout.php">
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+
+            <div class="container" id="content">
+              <h1>Admin</h1>
+              <small>Manage your chapter</small>
+
+              <p>Only Admins and Advisers can view this page</p>
+                 <footer style="position:bottom;">
+                      <div class="bg-blue color-white py-3">
+                        <center>
+                          <p>
+                            For more information, visit <a href="about.php" style="color:white;">The About Page</a>.
+                          </p>
+                          <p>
+                            Made by Team T1285, 2018-2019, All Rights Reserved
+                          </p>
+                        </center>
+                      </div>
+                    </footer>
+
+                    </html>
+
+                 <?php }
+
+
+
+                   ?>
